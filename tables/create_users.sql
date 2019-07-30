1c7992597ad3a0838e589709e3c007802ec3a343
N-152 <n-152@fs-admins-MacBook-Pro.local>
2019-07-30 15:57:04 +0800

    added foreign keys for tasks and users

--- tables/create_users.sql
+++ tables/create_users.sql
@@ -3,5 +3,6 @@ create table Users(
     UserID int primary key AUTO_INCREMENT,
     Username varchar(45),
     Password varchar(45),
-    RoleID int
+    RoleID int,
+    foreign key(RoleID) references Roles(RoleID)
 )
