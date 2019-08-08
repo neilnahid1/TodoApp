2078b7ae31d312108a0d8c3850ba3fc5aa7876d5
N-152 <n-152@fs-admins-MacBook-Pro.local>
2019-08-08 14:24:21 +0800

    initialized refactoring of table generation

---
+++ js/create_table.js
@@ -0,0 +1,46 @@
+function generateTable(JSONData){
+    /*
+    JSONData is assumed to have at least 1 row
+    */
+
+     var table = document.createElement("table");
+     //create a table header of of the JSONData
+     var headers = createTableHeadersFrom(JSONData);
+     //append each header to the table object.
+     headers.forEach(header => {
+         table.appendChild(header);
+     });
+    //  //populate the table with rows from JSONData
+      var rows = createRowsFrom(JSONData);
+      rows.forEach(row=>{
+          table.appendChild(row);
+      });
+    return table;
+}
+function createTableHeadersFrom(JSONData){
+    //creates an array of th elements
+    var tableHeaders = Object.keys(JSONData[0]).map(createTableHeader);
+    return tableHeaders;
+}
+function createTableHeader(name){
+    var th = document.createElement('th');
+    th.textContent = name;
+    return th;
+}
+function createRowsFrom(JSONData){
+    var tableRows = JSONData.map(createRow);
+    return tableRows;
+}
+function createRow(row){
+    //row is a JSON object
+    var tr = document.createElement("tr");
+    for(key in row){
+        var td = document.createElement("td");
+        td.textContent = row[key];
+        tr.appendChild(td);
+    }
+    return tr;
+}
+function setTableAttributes(...args){
+    args.map(alert);
+ }
