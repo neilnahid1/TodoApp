e6c9bd58c6d7201b33ac4bbfe99806aec684e4d9
N-152 <n-152@fs-admins-MacBook-Pro.local>
2019-08-08 17:39:55 +0800

    added sql file for my database

--- database.sql
+++ database.sql
@@ -48,14 +48,14 @@ DROP TABLE IF EXISTS `Tasks`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Tasks` (
   `TaskCodeID` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(45) DEFAULT NULL,
+  `Name` varchar(255) NOT NULL,
   `Description` text,
   `UserID` int(11) DEFAULT NULL,
   `IsComplete` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`TaskCodeID`),
   KEY `UserID` (`UserID`),
   CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`)
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,7 +64,7 @@ CREATE TABLE `Tasks` (
 
 LOCK TABLES `Tasks` WRITE;
 /*!40000 ALTER TABLE `Tasks` DISABLE KEYS */;
-INSERT INTO `Tasks` VALUES (32,'Shopping','Shopee',6,1);
+INSERT INTO `Tasks` VALUES (32,'Shopping','Shopee\r\nShoppee.\r\nbhahahahah\r\nhahahahaha\r\nthis is so bad..asdasdd',6,1),(36,'hello world','its me.',1,0),(37,'blabla','\'',1,0),(38,'blabla','\'\'\'\'',1,0),(39,'sup nibba','\'\'\';\';\'\'\'\'\'\'\'\'\'',1,0),(40,'with multiline','sdsd\r\nalsjdalksd\r\nasdasd',1,0);
 /*!40000 ALTER TABLE `Tasks` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -85,7 +85,7 @@ CREATE TABLE `Users` (
   UNIQUE KEY `Password` (`Password`),
   KEY `RoleID` (`RoleID`),
   CONSTRAINT `users_ibfk_1` FOREIGN KEY (`RoleID`) REFERENCES `roles` (`RoleID`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -107,4 +107,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-08-07 14:31:08
+-- Dump completed on 2019-08-08 17:38:48
