738e2bc1350c604023037e7f0798eb0c45fea4b9
Neil-Mac <neil.nahid@fullspeedtechnologies.com>
2019-08-14 14:52:32 +0800

    wtf

--- js/users.js
+++ js/users.js
@@ -1,27 +1,37 @@
-async function getUsers() {
-    document.getElementById("userTable").childNodes.forEach(node=>node.remove());
-    let table = await $.post("../php/users/process.php", { Type: "getUsers" }).then(res => { return generateTable(JSON.parse(res)) });
-    document.getElementById("userTable").appendChild(table);
-    $(document).ready(() => {
-        var tbl = $('#table').DataTable({ select: true });
-        $('#btn_editUser').click(e => {
-            let data = tbl.row({ selected: true }).data();
-            populateUserModalFields(data);
-        });
-        $('#btn_deleteUser').click(e => {
-            let data = tbl.row({ selected: true }).data();
-            deleteUser(data[0]);
-        });
+function getUsers() {
+    $.post("../php/users/process.php", { Type: "getUsers" },
+        (res) => {
+            let table = generateTable(JSON.parse(res));
+            document.getElementById("userTable").innerHTML = "";
+            document.getElementById("userTable").appendChild(table);
+            applyDataTables();
+        }
+    );
+}
+
+function applyDataTables() {
+    var tbl = $('#table').DataTable({ select: true, destroy: true });
+    $('#btn_editUser').click(e => {
+        let data = tbl.row({ selected: true }).data();
+        populateUserModalFields(data);
+    });
+    $('#btn_confirmDelete').click(e => {
+        let data = tbl.row({ selected: true }).data();
+        deleteUser(data[0]);
+    });
+    $('#btn_updateChanges').click(e => {
+        updateUserProfile();
     });
 }
 
 function updateUserProfile() {
     let formData = $('#userProfile').serializeArray();
-    $.post("../php/users/process.php", formData, (res) => {
+    $.post("../php/users/process.php", formData).then(res => {
         document.getElementById('response').innerHTML = res;
         if (res == "Successfully updated, redirecting now...") {
             window.location.href = "login.php";
         }
+        fetchUsersTable();
     });
 }
 
@@ -33,13 +43,12 @@ function deleteUser(UserID) {
     $.post("../php/users/process.php", { UserID: UserID, Type: "deleteUser" }).then((res) => {
         alert(res);
         if (res == "Successfully deleted!")
-            getUsers();
+            fetchUsersTable();
         else
             alert(res);
-
     });
 }
-/**
+/**q
  * 
  * @param {Array} data 
  */
